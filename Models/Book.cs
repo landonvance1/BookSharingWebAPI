5d3a74acc8a05e55c834139f7837b2112e41251a
unknown <landonpvance@gmail.com>
2025-08-16 11:07:18 -0500

    feat(search): added static file serving for thumbnails

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,5 +6,6 @@ namespace BookSharingApp.Models
         public string Title { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string ISBN { get; set; } = string.Empty;
+        public string ThumbnailUrl { get; set; } = string.Empty;
     }
 }
