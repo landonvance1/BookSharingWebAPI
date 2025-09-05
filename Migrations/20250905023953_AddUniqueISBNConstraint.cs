552c2e3eb7a876f85cd11afbd9271525c9a315ef
Landon Vance <landonpvance@gmail.com>
2025-09-04 21:45:18 -0500

    feat(book): made isbn unique in book model

---
+++ Migrations/20250905023953_AddUniqueISBNConstraint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookSharingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueISBNConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_book_isbn",
+                table: "book",
+                column: "isbn",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_book_isbn",
+                table: "book");
+        }
+    }
+}
