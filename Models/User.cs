b3e1108f44bc1ede64ac38091776d39a53f81faf
Landon Vance <landonpvance@gmail.com>
2025-09-13 13:43:33 -0500

    fix(user): jsonginored sensitive user fields to prevent api from returning them

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace BookSharingApp.Models
 {
@@ -19,5 +20,52 @@ namespace BookSharingApp.Models
         public string FullName => $"{FirstName} {LastName}";
         
         public ICollection<CommunityUser> JoinedCommunities { get; set; } = new List<CommunityUser>();
+        
+        // Hide sensitive properties from JSON serialization
+        [JsonIgnore]
+        public override string? PasswordHash { get; set; }
+        
+        [JsonIgnore]
+        public override string? SecurityStamp { get; set; }
+        
+        [JsonIgnore]
+        public override string? ConcurrencyStamp { get; set; }
+        
+        [JsonIgnore]
+        public override string? NormalizedUserName { get; set; }
+        
+        [JsonIgnore]
+        public override string? NormalizedEmail { get; set; }
+        
+        // Hide additional system/internal properties
+        [JsonIgnore]
+        public override string? UserName { get; set; }
+        
+        [JsonIgnore]
+        public override string? Email { get; set; }
+        
+        [JsonIgnore]
+        public override bool EmailConfirmed { get; set; }
+        
+        [JsonIgnore]
+        public override string? PhoneNumber { get; set; }
+        
+        [JsonIgnore]
+        public override bool PhoneNumberConfirmed { get; set; }
+        
+        [JsonIgnore]
+        public override bool TwoFactorEnabled { get; set; }
+        
+        [JsonIgnore]
+        public override DateTimeOffset? LockoutEnd { get; set; }
+        
+        [JsonIgnore]
+        public override bool LockoutEnabled { get; set; }
+        
+        [JsonIgnore]
+        public override int AccessFailedCount { get; set; }
+        
+        [JsonIgnore]
+        public override string Id { get; set; } = string.Empty;
     }
 }
